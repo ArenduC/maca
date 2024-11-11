beb1f0e1d25be331a038c34432986ce5c537860b
Arendu Chanda <arenduchanda1997@gmail.com>
2024-11-11 09:52:40 +0530

    firt add

---
+++ lib/connection/api_connection.dart
@@ -0,0 +1,5 @@
+const basUrl = "http://192.168.2.4:3000/maca/";
+
+class PostUrl {
+  String borderList = "${basUrl}v1/borderlist";
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:maca/connection/api_connection.dart';
+import 'package:maca/service/api_service.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  dynamic borderListData = [];
+
+  @override
+  void initState() {
+    borderList();
+    super.initState();
+  }
+
+  Future<void> borderList() async {
+    dynamic response = await ApiService()
+        .apiCallService(endpoint: PostUrl().borderList, method: "GET");
+
+    if (kDebugMode) {
+      if (response.statusCode == 200) {
+        dynamic data = jsonDecode(response.body);
+
+        print(data);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('maca'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Go to the next page',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Next page'),
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'This is the next page',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Home Page"),
+          GestureDetector(
+            onTap: () {
+              borderList();
+            },
+            child: const Text("call data"),
+          )
+        ],
+      )),
+    );
+  }
+}
